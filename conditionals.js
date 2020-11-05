dfa4aed84e569365320c8b12eaae4190525debb5
Cameron Glowicki <camglowicki@hotmail.com>
2020-11-05 05:24:53 +0000

    retry conditionals

--- conditionals.js
+++ conditionals.js
@@ -11,7 +11,7 @@ if (temperature < 0) {
 console.log("Now you're ready to go outside!");
 
 const isCitizen = true;
-const age = 26;
+const age = 36;
 
 if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
