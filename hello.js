e1b71c6249038812d978438970636886ebeeea24
Cameron <camglowicki@hotmail.com>
2020-11-07 21:06:28 +0000

    Basic function declaration practice

---
+++ hello.js
@@ -0,0 +1,11 @@
+let sayHello = function() {
+  console.log('Hello, world');
+}
+
+sayHello = function(name) {
+  console.log('Hello, ' + name);
+}
+
+sayHello('Cam')
+sayHello('Laura')
+sayHello('Shababa')
